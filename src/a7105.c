e4178e4d2a036aa16a501336de08fc9cafe818e8
Alexander Zakharov <sys@eml.ru>
2022-01-15 19:11:15 +0300

    update

--- src/a7105.c
+++ src/a7105.c
@@ -1,25 +1,69 @@
 #include "a7105.h"
 
 void A7105Init(void) {
+    A7105Reset();
+    /// After reset 16MHz crystal and 500Kbps data rate are already
+    /// configured. The default base frequency is then 2400.001454 MHz.
+    /// A channel width of 500KHz is recommended. 16MHz/(4*500KHz)-1 = 7,
+    /// leave all the other bits as they are.
+    a7105SpiRegWrite(A7105_10_PLL_II, 0xf0 | (7 << 1));
+
+    /// Note: To stay in the 2.4 to 2.483 GHz ISM band, only channels 1
+    /// through 166 should be used (with "Auto IF Exchange" enabled, master
+    /// sends on 1-166, slave sends on 0-165).
+
+    /**
+     * Current understanding of the datasheet (concerning Auto IF):
+     * Master sends on channel n, sets ULS=0 to receive on channel n-1.
+     * Slave then has to send on channel n-1, but sets ULS=1 to receive on
+     * channel n-1+1=n. Besides the bad english, the datasheet is also
+     * not clear about what to do if the channel width is not 500KHz, as
+     * ULS always shifts by 500KHz.
+     */
+
+    /// Demodulator DC estimation average mode: 0x1 is recommended.
+    /// Preamble pattern detection length: 0x2 recommended for 500Kbps
+    a7105SpiRegWrite(A7105_20_CODE_II, 0x4 | (0x1 << 4) | 0x2);
+
+    /// Demodulator DC estimation mode: Average and hold (0x2)
+    a7105SpiRegWrite(A7105_29_RX_DEM_TEST_I, 0x07 | (0x2 << 5));
+
+    /// Enable auto IF offset (shift frequency while receiving) and FIFO mode
+    a7105SpiRegWrite(A7105_01_MODE_CONTROL, (1 << 5) | (1 << 1));
+
+    /// Set BPF bandwidth to 500KHz (bits 6 and 5 shall always be set to 1)
+    /// Although it always reads 0, we assume 500KHz is already selected
+    /// after reset.
+    /// a7105SpiRegWrite(A7105_18_RX, (0x3 << 5) | 0x02);
+
+    /// Disable FIFO extension (FPM = 0) and segmentation (PSA = 0)
+    a7105SpiRegWrite(A7105_04_FIFO_II, 0);
+
+    /// Set FIFO size to 1 (for testing only)
+    a7105SpiRegWrite(A7105_03_FIFO_I, 0);
 
 }
 
 uint8_t A7105Reset(void) {
     uint8_t RESETN = 0x00; ///< Write to this register by 0x00 to issue reset command, then it is auto clear
-
+    uint8_t result;
     a7105SpiRegWrite(A7105_00_MODE, RESETN);
     HAL_Delay(1000);
 
     /// Set both GPIO as output and low
     A7105SetTxRxMode(TXRX_OFF);
-    uint8_t result = a7105SpiRegRead(A7105_10_PLL_II) == 0x9E;
+    result = a7105SpiRegRead(A7105_10_PLL_II) == 0x9E;
     a7105SpiStrobe(A7105_STROBE_STANDBY);
 
     return result;
 }
 
+void A7105SetChannel(uint8_t chnl) {
+    a7105SpiRegWrite(A7105_0F_PLL_I, chnl);
+}
+
 void A7105SetTxRxMode(enum TXRX_State mode) {
-    if(mode == TX_EN) {
+    if (mode == TX_EN) {
         a7105SpiRegWrite(A7105_0B_GPIO1_PIN_I, 0x33);
         a7105SpiRegWrite(A7105_0C_GPIO2_PIN_II, 0x31);
     } else if (mode == RX_EN) {
@@ -34,8 +78,6 @@ void A7105SetTxRxMode(enum TXRX_State mode) {
         a7105SpiRegWrite(A7105_0C_GPIO2_PIN_II, 0x33);
     }
 }
-
-
 
 uint8_t A7105Calib(void) {
     uint8_t retVal = 0;
@@ -90,6 +132,6 @@ uint8_t A7105Calib(void) {
     return retVal;
 }
 
-void setPower(uint8_t PAC_reg, uint8_t TBG_reg) {
+void A7105SetPower(uint8_t PAC_reg, uint8_t TBG_reg) {
     a7105SpiRegWrite(A7105_28_TX_TEST, (PAC_reg << 3) | TBG_reg);
 }
--- src/a7105_spi.c
+++ src/a7105_spi.c
@@ -21,10 +21,49 @@ void setA7105_GIO2(GPIO_TypeDef *GIO2_PORT, uint16_t GIO2_PIN) {
     conf.GIO2_PIN = GIO2_PIN;
 }
 
-void a7105SpiStrobe(enum A7105_Strobe data) {
+void a7105SpiStrobe(A7105_Strobe data) {
     HAL_GPIO_WritePin(conf.SCS_PORT, conf.SCS_PIN, GPIO_PIN_RESET);
-    HAL_SPI_Transmit(conf.spi, (uint8_t*) data, 1, A7105_SPI_TIMEOUT);
+    HAL_SPI_Transmit(conf.spi, (uint8_t *) &data, 1, A7105_SPI_TIMEOUT);
     HAL_GPIO_WritePin(conf.SCS_PORT, conf.SCS_PIN, GPIO_PIN_SET);
+}
+
+void a7105SpiSetId(uint32_t id) {
+    uint8_t data[4];
+    HAL_GPIO_WritePin(conf.SCS_PORT, conf.SCS_PIN, GPIO_PIN_RESET);
+
+    uint8_t reg = A7105_06_ID_DATA;
+    HAL_SPI_Transmit(conf.spi, &reg, 1, A7105_SPI_TIMEOUT);
+
+    data[0] = (id >> 24) & 0xFF;
+    data[1] = (id >> 16) & 0xFF;
+    data[2] = (id >> 8) & 0xFF;
+    data[3] = (id >> 0) & 0xFF;
+
+    HAL_SPI_Transmit(conf.spi, data, 4, A7105_SPI_TIMEOUT);
+
+    HAL_GPIO_WritePin(conf.SCS_PORT, conf.SCS_PIN, GPIO_PIN_SET);
+}
+
+uint32_t a7105SpiGetId(void) {
+    uint32_t ID;
+    uint8_t idBytes[4];
+
+    HAL_GPIO_WritePin(conf.SCS_PORT, conf.SCS_PIN, GPIO_PIN_RESET);
+    /// We could read this straight into ID, but the result would depend on
+    /// the endiness of the architecture.
+
+    uint8_t reg = A7105_06_ID_DATA;
+    HAL_SPI_Transmit(conf.spi, &reg, 1, A7105_SPI_TIMEOUT);
+    HAL_SPI_Receive(conf.spi, idBytes, 4, A7105_SPI_TIMEOUT);
+    HAL_GPIO_WritePin(conf.SCS_PORT, conf.SCS_PIN, GPIO_PIN_SET);
+
+    ID = (uint32_t) idBytes[0] << 24;
+    ID |= (uint32_t) idBytes[1] << 16;
+    ID |= (uint32_t) idBytes[2] << 8;
+    ID |= (uint32_t) idBytes[3] << 0;
+
+
+    return (ID);
 }
 
 void a7105SpiRegWrite(uint8_t address, uint8_t data) {
@@ -51,4 +90,28 @@ uint8_t a7105SpiRegRead(uint8_t reg) {
     HAL_GPIO_WritePin(conf.SCS_PORT, conf.SCS_PIN, GPIO_PIN_SET);
 
     return data[0];
+}
+
+void A7105SpiWriteData(uint8_t *data, size_t len, uint8_t channel) {
+    HAL_GPIO_WritePin(conf.SCS_PORT, conf.SCS_PIN, GPIO_PIN_RESET);
+
+    uint8_t reg = A7105_STROBE_RST_WRPTR;
+    HAL_SPI_Transmit(conf.spi, &reg, 1, A7105_SPI_TIMEOUT);
+    reg = A7105_05_FIFO_DATA;
+    HAL_SPI_Transmit(conf.spi, &reg, 1, A7105_SPI_TIMEOUT);
+
+    /// transmit data
+    HAL_SPI_Transmit(conf.spi, data, len, A7105_SPI_TIMEOUT);
+
+    HAL_GPIO_WritePin(conf.SCS_PORT, conf.SCS_PIN, GPIO_PIN_SET);
+    a7105SpiRegWrite(A7105_0F_PLL_I, channel);
+    a7105SpiStrobe(A7105_STROBE_TX);
+}
+
+void A7105SpiReadData(uint8_t *buffer, size_t len) {
+    a7105SpiStrobe(A7105_STROBE_RST_RDPTR);
+
+    for (size_t i = 0; i < len; i++) {
+        buffer[i] = a7105SpiRegRead(A7105_05_FIFO_DATA);
+    }
 }
